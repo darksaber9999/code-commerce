19f4f492ef3719f99078a9fb263f8da6ed13ceec
Chris <77562903+darksaber9999@users.noreply.github.com>
2022-10-10 16:45:48 -0600

    Remove console log from Confirm "Track" button

--- src/js/components/Confirm/Confirm.jsx
+++ src/js/components/Confirm/Confirm.jsx
@@ -18,7 +18,7 @@ class Confirm extends React.Component {
         <div>
           <div>Confirmation</div>
           <div className="btn">
-            <button onClick={console.log('click')}>Track Order</button>
+            <button>Track Order</button>
             <button onClick={this.goToStore}>Back to Store</button>
           </div>
         </div>
